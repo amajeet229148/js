5c7ae3c3a8001b84e01b499ccbe63171d388430f
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2023-09-22 13:57:28 +0530

    Add files via upload

---
+++ amar1/assets/array method.js
@@ -0,0 +1,21 @@
+//let arr = [1,2,3,4,5,6,8]
+//let newarr = arr.map((number,index)=>{
+ //   if (index %2==0)
+
+   // return number*5
+//})
+//console.log(newarr)
+
+//let  arr = [1,2,3,4,5,6,8,7,]
+//let EvenNums =arr.folter((num,filter)) => {
+  //  if Number(%2===0) {
+    //    return num * 5;  
+    //}
+
+//}
+
+let arr = [1,2,3,4,5,6,7,8]
+let sum = arr.reduce((acc,sum,index)=>{
+    return acc+num
+},0)
+console.log(sum)
